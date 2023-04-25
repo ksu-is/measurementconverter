c3bea7d1a0e6f22c46e00eb9022d9c4d5b97aef5
lorentzchu <122303493+lorentzchu@users.noreply.github.com>
2023-04-24 23:29:22 -0400

    Update measurementconverter.py

--- measurementconverter.py
+++ measurementconverter.py
@@ -1,1 +1,9 @@
 
+
+input_measurement = input("Enter measurement unit:")
+
+
+measurement_value=input("Enter value:")
+
+
+convert_measurement=input("Enter measurement unit you wish to convert to:")
