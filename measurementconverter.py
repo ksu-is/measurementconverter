08675adf751582af323ec55e35ff5eff11f9e079
lorentzchu <122303493+lorentzchu@users.noreply.github.com>
2023-04-27 18:30:57 -0400

    created mainframe of the software

--- measurementconverter.py
+++ measurementconverter.py
@@ -1,9 +1,28 @@
+# Importing the modules
+import tkinter as tk
+from tkinter.constants import UNITS
+import tkinter.font as font
+from functools import partial
+from tkinter import StringVar, messagebox
+from tkinter import ttk
 
-#asks user to enter measurement unit
-input_measurement = input("Enter measurement unit:")
+# Creating the window
+window  =tk.Tk()
+window.geometry('500x600')
+window.title('Measurement Converter')
+window.configure(bg = 'peach puff2')
 
-#enter value 
-measurement_value=input("Enter value:")
 
-#enter measurement you wish to convert to
-convert_measurement=input("Enter measurement unit you wish to convert to:")
+# Creating the fonts
+font1 = font.Font(family = 'helvetica',size = '30')
+font2 = font.Font(family = 'helvetica',size = '10')
+font3 = font.Font(family = 'helvetica',size = '20')
+
+# creating unit converter label
+main = tk.Label(window,text = 'Measurement Converter',bg = 'peach puff2')
+main['font'] = font1
+main.place(relx = '0.48',rely ='0.1', anchor = 'center')
+
+#creating the mainloop
+window.mainloop()
+
